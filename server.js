c65a1baa20a82ddcdc6be107f4a2f9ad4cb048aa
Luis Emilio Nathanael De la rosa Meneses <dluisemilionathanael@gmail.com>
2025-11-13 17:45:26 -0600

    update

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.get('/', (req, res) => {
+  res.send("Welcome to the W03 Project API");
+});
+
 
 // Routers
 const bookRoutes = require('./routes/books');

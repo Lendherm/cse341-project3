2f56b0c5c50fd376d620872e5338e09ecb19b500
Luis Emilio Nathanael De la rosa Meneses <dluisemilionathanael@gmail.com>
2025-11-13 17:29:48 -0600

    update

--- swagger.js
+++ swagger.js
@@ -8,7 +8,16 @@ const options = {
       version: '1.0.0',
       description: 'CRUD API for Books and Authors'
     },
-    servers: [{ url: 'http://localhost:8080' }]
+    servers: [
+      {
+        url: 'http://localhost:8080',
+        description: 'Local server'
+      },
+      {
+        url: 'https://cse341-project3-11r5.onrender.com',
+        description: 'Render deployment'
+      }
+    ]
   },
   apis: ['./routes/*.js']
 };
